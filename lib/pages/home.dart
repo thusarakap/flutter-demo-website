707320489bd6161b09e57ac2f67cf24584cefd62
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-09-03 17:54:26 +0530

    Create home.dart

---
+++ lib/pages/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../components/nav_bar.dart';
+import '../components/hero_section.dart';
+import '../components/about_us_section.dart';
+import '../components/footer.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: NavBar(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeroSection(),
+            AboutUsSection(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Footer(),
+    );
+  }
+}
